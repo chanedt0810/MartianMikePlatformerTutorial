7a3f4baed481ca31652f6fb576efaceb3da88292
Chane Du Toit <chanedt1008@gmail.com>
2025-05-04 20:01:38 +0200

    Add level time management and timer functionality to levels

--- scripts/level.gd
+++ scripts/level.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 @export var next_level: PackedScene = null
+@export var level_time = 5
 
 @onready var deathZone = $DeathZone
 @onready var start = $Start
@@ -9,6 +10,9 @@ extends Node2D
 @onready var spikeBall = $Traps/SpikeBall
 
 var player = null
+var timer_node = null
+var time_left
+var win = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -28,6 +32,15 @@ func _ready() -> void:
 
 	# Connect the exit to the player
 	exit.body_entered.connect(_on_exit_body_entered)
+
+	time_left = level_time
+
+	timer_node = Timer.new()
+	timer_node.name = "Level Timer"
+	timer_node.wait_time = 1
+	timer_node.timeout.connect(_on_level_timer_timeout)
+	add_child(timer_node)
+	timer_node.start()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
@@ -55,5 +68,15 @@ func _on_exit_body_entered(body: Node) -> void:
 		if next_level != null:
 			exit.animate()
 			player.active = false
+			win = true
 			await get_tree().create_timer(1.5).timeout
 			get_tree().change_scene_to_packed(next_level)
+
+func _on_level_timer_timeout() -> void:
+	if win == false:
+		time_left -= 1
+		print("Time left: ", time_left)
+		if time_left <= 0:
+			# Time is up, reset the player
+			reset_player()
+			time_left = level_time
